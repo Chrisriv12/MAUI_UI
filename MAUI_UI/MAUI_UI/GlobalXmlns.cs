f613ab5dc7a58354c4a83c76c30c022acba16f03
Chrisriv12 <chrriv7477@students.ecpi.edu>
2025-09-08 10:27:54 -0400

    Added new button fuctions

---
+++ MAUI_UI/MAUI_UI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUI_UI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUI_UI.Pages")]
---
+++ MAUI_UI/MAUI_UI/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MAUI_UI
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDisplayClicked(object sender, EventArgs e)
+        {
+
+            lblOutput.Text = txtInput.Text;
+        }
+        private void OnResetClicked(object? sender, EventArgs e)
+        {
+            lblOutput.Text = string.Empty;
+            txtInput.Text = string.Empty;
+        }
+    }
+}
